81877417f3115bf103dd2aa536b8e656297347f8
Christoph Schneider <Christoph.Schneider@dci-student.org>
2025-11-07 12:29:22 +0100

    fix: Attempt to fix static files not delivered

--- backend/build.sh
+++ backend/build.sh
@@ -3,7 +3,7 @@ set -o errexit  # exit on errors
 
 # cd backend
 pip install -r requirements.txt
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]]; then
